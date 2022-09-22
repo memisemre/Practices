8aafa01039737dede5f747481c278e1b339d25ee
memisemre <dev.emrememis@gmail.com>
2022-09-22 13:18:45 +0300

    1

---
+++ app.js
@@ -0,0 +1,43 @@
+//Obje Nedir ? İçerisinde birden fazla veri veya veri tipi depolayan yapılardır. 
+// JSON bir veri tutma şeklidir. 
+// JSON objeleri array'dir. 
+// let a;
+// a = "osman"; //String tipinde veri içeren bir obje;
+// a = 3; //Number tipinde veri içeren bir obje;
+// a = [1,2,3,4,"5"] //Array
+// a = {
+//     name: "mahmut",
+//     surname: "osmanoğullarından",
+//     age: 31,
+//     job: "student",
+//     address: {
+//         city:"gaziantep",
+//         street: "şahinbey"
+//     },
+//     workingStatus : function(){
+//         console.log(tihs.name);
+//     }
+// }
+// console.log(a.workingStatus())
+
+// let exampleJSONObject = [
+//     {
+//         name: "Osman",
+//         age: 31
+//     },
+//     {
+//         name: "Mahmut",
+//         age: 31
+//     }
+// ]
+// console.log(exampleJSONObject[0].name)
+// let nullObject = null;
+// console.log(nullObject)
+// document.querySelector('.btn').addEventListener('click', function(){
+//     this.style.color= "red";
+// })
+//Array Destriction;
+// let names = ["emre","mahmut","osman"]
+// let [name1,name2,name3,] = names;
+// console.log(names)
+// console.log("name1:" ,name1,"name2:",name2,"name3:",name3)
