ef87ec2c6ee593c8fc5a5ee6aef725dae5d54289
memisemre <dev.emrememis@gmail.com>
2022-09-22 18:16:06 +0300

    Add attempt

--- Array Iteration/app.js
+++ Array Iteration/app.js
@@ -70,3 +70,46 @@
 // console.log(numbers.join())
 // let stringArrayExample = ["ahmet","emre","osman"]
 // document.querySelector('.text').textContent = stringArrayExample.join("")
+let data = {
+    "squadName": "Super hero squad",
+    "homeTown": "Metro City",
+    "formed": 2016,
+    "secretBase": "Super tower",
+    "active": true,
+    "members": [
+      {
+        "name": "Molecule Man",
+        "age": 29,
+        "secretIdentity": "Dan Jukes",
+        "powers": [
+          "Radiation resistance",
+          "Turning tiny",
+          "Radiation blast"
+        ]
+      },
+      {
+        "name": "Madame Uppercut",
+        "age": 39,
+        "secretIdentity": "Jane Wilson",
+        "powers": [
+          "Million tonne punch",
+          "Damage resistance",
+          "Superhuman reflexes"
+        ]
+      },
+      {
+        "name": "Eternal Flame",
+        "age": 1000000,
+        "secretIdentity": "Unknown",
+        "powers": [
+          "Immortality",
+          "Heat Immunity",
+          "Inferno",
+          "Teleportation",
+          "Interdimensional travel"
+        ]
+      }
+    ]
+  }
+let entriesData = Object.entries(data);
+document.querySelector('.text').textContent = entriesData;
