fbb1e4a0d15c3b65e479f59a47afeaa99a597669
memisemre <dev.emrememis@gmail.com>
2022-09-22 16:32:11 +0300

    add attempts

--- Array Iteration/app.js
+++ Array Iteration/app.js
@@ -1,7 +1,47 @@
-let numbersArrayOne = [1,2,3,4,5,6,7]
-let numbersArrayTwo = numbersArrayOne.map(myFunction)
-document.querySelector('.text-area').innerHTML = `${numbersArrayTwo}`
-function myFunction(value){
-    return value*2
-}
-console.log(numbersArrayTwo)
+let numbersArrayOne;
+numbersArrayOne = [1,2,3,4,5,6,7]
+let numbersArrayTwo;
+// numbersArrayTwo = numbersArrayOne.map(mapFunction)
+// function mapFunction(value){
+//     return value*2
+// }
+// numbersArrayTwo = numbersArrayOne.filter(filterFunction)
+// function filterFunction(value){
+//     return value>=2
+// }
+// let stringsArrayOne = ["Mahmut","ata","bak"];
+// let stringsArrayTwo = stringsArrayOne.reduce(reduceFunction)
+// function reduceFunction(sentence,value){
+//     return sentence+" "+value
+// }
+// numbersArrayOne = [123,321,456]
+// numbersArrayTwo = numbersArrayOne.reduce(numberReduceFunction)
+// function numberReduceFunction(sum,value){
+//     return sum + value}
+
+// console.log(numbersArrayTwo)
+// console.log(typeof numbersArrayTwo)
+// document.querySelector('.text').textContent = `${numbersArrayTwo}`
+// let test = [numbersArrayTwo+1]
+// console.log(test)
+// console.log(typeof test) 
+// console.log(stringsArrayTwo)
+// console.log(typeof stringsArrayTwo)
+// let numbersArray = [0,65,34,97,56,6,65];
+// let numbersArrayTwo = numbersArray.filter(function(value){
+//     return value > 31; 
+// })
+let numbersArray = [0,65,34,97,56,6,65];
+// let numbersArrayTwo = numbersArray.filter((value)=>{
+//     return value>31
+// })
+// let numbersArrayTwo = numbersArray.find((value) =>{
+//     return value>31
+// })
+// numbersArrayTwo = numbersArray.some((x) => x<10)
+
+
+
+
+document.querySelector('.text').textContent = `${numbersArrayTwo}`
+
